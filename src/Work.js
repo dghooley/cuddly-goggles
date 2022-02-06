475d7950ea8b2e9e82a32359fb3957ea85887b57
Derick Hooley <dghooley@gmail.com>
2022-02-06 14:54:43 -0600

    addedWorkSectionToResume

--- src/Work.js
+++ src/Work.js
@@ -1,6 +1,28 @@
 import React from "react";
+import "./work.css";
 
 export default function Work() {
+    return (
+        <>
+            <div className="work-inside-new mx-2 my-4">
+                <div className="d-flex flex-column">
+                    <div className="d-flex flex-row justify-content-between">
+                        <span className="company-name">Self Employed</span>
+                        <span className="year-passedout">2021-present</span>
+                    </div>
+                </div>
 
-    return <>Work</>;
+                <div className="d-flex flex-column">
+                    <span className="position">Full Stack Developer</span>
+                    <span className="description-position">
+                        <ul>
+                            <li> Created multiple applications using MERN stack</li>
+                            <li> Created multiple applications using MERN stack</li>
+                            <li> Created multiple applications using MERN stack</li>
+                        </ul>
+                    </span>
+                </div>
+            </div>
+        </>
+    );
 }
