1a03ede801f0eaefdf7f65311fa6e0eae90e1e42
Derick Hooley <dghooley@gmail.com>
2022-02-06 15:23:12 -0600

    projects/programmingSections

--- src/Programming.js
+++ src/Programming.js
@@ -1,6 +1,50 @@
 import React from "react";
+import "./programming.css";
 
 export default function Programming() {
-
-    return <>Programming</>;
+    const value = [
+        {
+            text: "javascript",
+            percentage: 80
+        },
+        {
+            text: "MongoDB",
+            percentage: 80
+        },
+        {
+            text: "Express",
+            percentage: 80
+        },
+        {
+            text: "Node JS",
+            percentage: 80
+        },
+        {
+            text: "React JS",
+            percentage: 80
+        }
+    ];
+    return (
+        <>
+            <div>
+                <div className="row">
+                    {value.map((value, index) => {
+                        return (
+                            <>
+                                <div className="col-lg-6 col-md-6 col-sm-12 my-2">
+                                    <span className="language">{value.text} </span>
+                                    <div className="progress-some">
+                                        <div
+                                            className="progress-new"
+                                            style={{ width: `${value.percentage}%` }}
+                                        ></div>
+                                    </div>
+                                </div>
+                            </>
+                        );
+                    })}
+                </div>
+            </div>
+        </>
+    );
 }
